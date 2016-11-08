5acf6e987392342846f608581bf67a1759448b63
alessandro_gsantos <T&(39(#2122>
2016-11-08 16:33:01 -0200

    first commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef _WIN32
+#include<curses.h> //Biblioteca para linux
+#else
+#include <conio.h> //Biblioteca para windows
+#endif
+
+int main(){
+    readFile();
+    return 1;
+}
+
+void readFile() {
+   char buffer[1024] ;
+   char *record,*line;
+   int i=0,j=0;
+   int mat[5][3];
+   FILE *fstream = fopen("input","r");
+
+   while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL)
+   {
+     record = strtok(line,",");
+     while(record != NULL) {
+         printf("%s",record) ;    //here you can put the record into the array as per your requirement.
+         mat[i][j++] = atoi(record) ;
+         record = strtok(NULL,",");
+     }
+     ++i ;
+   }
+}
